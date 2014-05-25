2d13b99054e916ff7f6ea55d448afe6cc960eb6c
Taku Unno <taku_unno@dwango.co.jp>
2014-05-25 16:38:20 +0900

    page_redirector.php
    .htaccessを設定

--- htdocs/urlmask/rd/page_redirector.php
+++ htdocs/urlmask/rd/page_redirector.php
@@ -1,8 +1,8 @@
 <?php // -*- encoding utf-8 -*-
 
-require_once(__DIR__ . '/../../controllers/PageRedirectorController.php');
+require_once(__DIR__ . '/../../../controllers/PageRedirectorController.php');
 
-$conf_arr = parse_ini_file(__DIR__ . '/../../conf/urlmask.ini', true);
+$conf_arr = parse_ini_file(__DIR__ . '/../../../conf/urlmask.ini', true);
 $env = '';
 if(isset($conf_arr['environment']['env']) === true)
 {
